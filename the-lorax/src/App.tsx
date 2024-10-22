46504f19b8dcb4f679d7ad14488d315ebb1cdbfd
ntnguyen59 <jennynguyen050903@gmail.com>
2024-10-22 19:20:18 -0400

    Updated App for the Action button

--- the-lorax/src/App.tsx
+++ the-lorax/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Intro from './app/page';
+import BackgroundComponent from "./app/landing_page";
+import DirectoryButton from "./app/action_page"
 
 function App() {
+  const handleButtonClick = () => {
+    alert("Custom Button Clicked!");
+  };
   return (
     <div className="App">
-      <Intro/>
+      {/* <p>alskdjf</p> */}
+      <BackgroundComponent/>
+      <DirectoryButton onClick={handleButtonClick} label="directory"/>
     </div>
   );
 }
