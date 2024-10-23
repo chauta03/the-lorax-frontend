1a667790d33e668cb46ef50d6cda7b9f18f6ccde
ntnguyen59 <jennynguyen050903@gmail.com>
2024-10-23 09:46:01 -0400

    Updated the main app to match the addition of action button

--- the-lorax/src/App.tsx
+++ the-lorax/src/App.tsx
@@ -2,17 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BackgroundComponent from "./app/landing_page";
-import DirectoryButton from "./app/action_page"
+import ActionButton from './app/action_page';
 
 function App() {
-  const handleButtonClick = () => {
-    alert("Custom Button Clicked!");
-  };
+  
   return (
     <div className="App">
-      {/* <p>alskdjf</p> */}
       <BackgroundComponent/>
-      <DirectoryButton onClick={handleButtonClick} label="directory"/>
+      <ActionButton/>
     </div>
   );
 }
