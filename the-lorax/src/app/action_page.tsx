86268a8b3999795b17dc3ce6b8525bda96911c9b
ntnguyen59 <jennynguyen050903@gmail.com>
2024-10-23 11:09:12 -0400

    Adding tree icon for the action page

--- the-lorax/src/app/action_page.tsx
+++ the-lorax/src/app/action_page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./action_page.css";
 import DirectoryButton from './action_buttons/directory_button';
 import AboutButton from './action_buttons/about_button';
+import TreeButton from './action_buttons/tree_button';
 
 const ActionButton: React.FC = () => {
     const Directory_Click = () => {
@@ -12,10 +13,15 @@ const ActionButton: React.FC = () => {
         alert("About");
     };
 
+    const Tree_Click = () => {
+        alert("Tree!!!!");
+    };
+
     return (
         <div className="action-page-background">
             <DirectoryButton onClick={Directory_Click} /> 
             <AboutButton onClick={About_Click} />
+            <TreeButton onClick={Tree_Click} />
         </div>
         
     );
