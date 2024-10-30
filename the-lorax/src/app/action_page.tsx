d4c37e2b2d7ec971843db3eaf1d717250f1dc346
ntnguyen59 <jennynguyen050903@gmail.com>
2024-10-30 15:43:33 -0400

    Update button orientations

--- the-lorax/src/app/action_page.tsx
+++ the-lorax/src/app/action_page.tsx
@@ -3,6 +3,7 @@ import "./action_page.css";
 import DirectoryButton from './action_buttons/directory_button';
 import AboutButton from './action_buttons/about_button';
 import TreeButton from './action_buttons/tree_button';
+import AdminButton from './action_buttons/admin_button';
 
 const ActionButton: React.FC = () => {
     const Directory_Click = () => {
@@ -17,11 +18,16 @@ const ActionButton: React.FC = () => {
         alert("Tree!!!!");
     };
 
+    const Admin_Click = () => {
+        alert("Tree!!!!");
+    };
+
     return (
         <div className="action-page-background">
             <DirectoryButton onClick={Directory_Click} /> 
             <AboutButton onClick={About_Click} />
             <TreeButton onClick={Tree_Click} />
+            <AdminButton onClick={Admin_Click} />
         </div>
         
     );
