322b55c732b5d348d9ff530a421cb5b0476a776a
tachau03 <taminhchau8903@gmail.com>
2024-10-31 23:12:20 -0400

    Update some buttons

--- the-lorax/src/app/action_buttons/aboutButton.tsx
+++ the-lorax/src/app/action_buttons/aboutButton.tsx
@@ -8,7 +8,7 @@ type CustomButtonProps = {
 
 const AboutButton: React.FC<CustomButtonProps> = ({onClick}) => {
     return (
-        <div className="button" style={{ display: 'inline-block' }}>
+        <div className="button">
             <svg xmlns="http://www.w3.org/2000/svg" width="310" height="330" viewBox="0 0 310 330" fill="none" onClick={onClick}>
                 <path d="M3.73463 314.22L48.4764 12.7C49.3159 7.04246 54.7209 3.2403 60.3293 4.36198L297.99 51.8941C303.224 52.9409 306.713 57.9139 305.917 63.1919L267.591 317.18C266.853 322.072 262.65 325.688 257.703 325.688H13.6263C7.51901 325.688 2.8382 320.262 3.73463 314.22Z" fill="#458244" stroke="#E0FFDF" stroke-width="7" stroke-dasharray="14 28 42 56"/>
                 <text 
--- the-lorax/src/app/action_buttons/treeButton.tsx
+++ the-lorax/src/app/action_buttons/treeButton.tsx
@@ -9,11 +9,11 @@ type CustomButtonProps = {
 
 const TreeButton: React.FC<CustomButtonProps> = ({onClick}) => {
     return (
-        <div className="button-tree-only" style={{position: 'relative', display: 'inline-block'}}>
+        <div className="button-tree-only" onClick={onClick}>
             <svg xmlns="http://www.w3.org/2000/svg" width="183" height="274" viewBox="0 0 183 274" fill="none" onClick={onClick} style={{position: 'absolute'}}>
                 <path d="M3.58824 258.27L38.7277 12.933C39.5329 7.31153 44.8521 3.48798 50.4371 4.51607L170.848 26.6813C176.002 27.6299 179.549 32.3958 178.978 37.6046L154.539 260.777C153.983 265.848 149.7 269.688 144.598 269.688H13.4872C7.40026 269.688 2.72522 264.296 3.58824 258.27Z" fill="#BCE8BB" stroke="#E0FFDF" stroke-width="7" stroke-dasharray="14 28 42 56"/>
             </svg>
-            <div className='tree-icon' style={{position: 'absolute'}}></div>
+            <div className='tree-icon'></div>
         </div>
     );
 };
