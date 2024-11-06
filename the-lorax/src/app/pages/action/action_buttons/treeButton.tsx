09eeb461b752e782b5446f8ca7863bdd3978d034
tachau03 <taminhchau8903@gmail.com>
2024-11-06 11:27:39 -0500

    Change directory of buttons

--- the-lorax/src/app/pages/action/action_buttons/treeButton.tsx
+++ the-lorax/src/app/pages/action/action_buttons/treeButton.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import "../action_page.css";
 import { relative } from 'path';
-import tree from '../../images/icons/tree-icon-3-1.svg';
-import { ReactComponent as TreeBackground } from '../../images/buttons/treeButtonBackground.svg';
+import tree from '../../../../images/icons/rotated-tree-icon-3-1.svg';
+import { ReactComponent as TreeBackground } from '../../../../images/buttons/treeButtonBackground.svg';
 
 
 // Create the functional component
@@ -13,7 +13,7 @@ type CustomButtonProps = {
 const TreeButton: React.FC<CustomButtonProps> = ({onClick}) => {
     return (
         <div className="button-tree-only" onClick={onClick}>
-            <img src={tree} alt="Tree Icon" className="tree-icon" />
+            <img src={tree} alt="Tree Icon" className="button-tree-icon" />
             <TreeBackground className="button-tree-background" />
         </div>
     );
