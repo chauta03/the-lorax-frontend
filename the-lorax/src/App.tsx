4d5e25e3e2b0692eb2ac96cc8654d5f8e88229fb
tachau03 <taminhchau8903@gmail.com>
2024-11-06 11:29:02 -0500

    Edit main app

--- the-lorax/src/App.tsx
+++ the-lorax/src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BackgroundComponent from "./app/landing_page";
-import ActionButton from './app/action_page';
+import BackgroundComponent from "./app/pages/landing/landing_page";
+import ActionButton from './app/pages/action/action_page';
 import Intro from './app/page'
+import Directory from './app/pages/directory/directory';
 
 function App() {
-  
+
   return (
     <div className="App">
-      <BackgroundComponent/>
-      <ActionButton/>
-      <Intro/>
+      <BackgroundComponent />
+      <ActionButton />
+      <Intro />
+      <Directory />
     </div>
   );
 }
