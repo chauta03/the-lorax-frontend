0408aec1aaa8b1b54fe6fd97b93a3ec999ac7a1e
tachau03 <taminhchau8903@gmail.com>
2024-11-06 11:27:52 -0500

    edit action_page

--- the-lorax/src/app/pages/action/action_page.tsx
+++ the-lorax/src/app/pages/action/action_page.tsx
@@ -4,6 +4,7 @@ import DirectoryButton from './action_buttons/directoryButton';
 import AboutButton from './action_buttons/aboutButton';
 import TreeButton from './action_buttons/treeButton';
 import SearchBar from './action_buttons/searchBar';
+import AdminButton from './action_buttons/adminButton';
 
 const ActionButton: React.FC = () => {
     const Directory_Click = () => {
@@ -18,21 +19,27 @@ const ActionButton: React.FC = () => {
         alert("Tree!!!!");
     };
 
+    const AdminButton_Click = () => {
+        alert("SearchBar!!!!");
+    };
+
     const SearchBar_Click = () => {
         alert("SearchBar!!!!");
     };
 
+
     return (
         <div className="action-page-background">
             <div className="action-page-upper-buttons">
-                <DirectoryButton onClick={Directory_Click} /> 
+                <DirectoryButton onClick={Directory_Click} />
                 <AboutButton onClick={About_Click} />
                 <TreeButton onClick={Tree_Click} />
+                <AdminButton onClick={AdminButton_Click} />
             </div>
-            <SearchBar onClick={SearchBar_Click}/>
+            <SearchBar onClick={SearchBar_Click} />
         </div>
-        
+
     );
 };
-  
+
 export default ActionButton;
