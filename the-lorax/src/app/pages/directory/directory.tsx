fc7790b2180612667b9814d0ee3b34f2d66c6987
tachau03 <taminhchau8903@gmail.com>
2024-11-11 14:55:15 -0500

    Add more in directory

--- the-lorax/src/app/pages/directory/directory.tsx
+++ the-lorax/src/app/pages/directory/directory.tsx
@@ -3,15 +3,15 @@
 import { useEffect, useRef, useState } from "react";
 import './directory.css';
 import Header from '../../components/header'
-import Filter from './components/filter'
-import DisplayFilter from "./components/displayFilter";
+import Filter from './components/sort'
+import DisplayFilter from "./components/displaySort";
 
 
 export default function Directory() {
 
     return (
         <div className="directory">
-            <Header/>
+            <Header />
             <div className="directory-body">
                 <DisplayFilter />
             </div>
