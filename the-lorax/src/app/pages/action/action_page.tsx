75e063d7dbebd98cf7eb8dce86770d951b42a140
ntnguyen59 <jennynguyen050903@gmail.com>
2024-11-11 22:27:04 -0500

    Action Page with mobile layout

--- the-lorax/src/app/pages/action/action_page.tsx
+++ the-lorax/src/app/pages/action/action_page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useEffect, useState} from 'react';
 import "./action_page.css";
 import DirectoryButton from './action_buttons/directoryButton';
 import AboutButton from './action_buttons/aboutButton';
@@ -7,6 +8,8 @@ import SearchBar from './action_buttons/searchBar';
 import AdminButton from './action_buttons/adminButton';
 import MapButton from './action_buttons/mapButton';
 import SupportButton from './action_buttons/supportButton';
+import AdminButtonMobile from './mobile_action_buttons/adminButtonMobile';
+import TreeButtonMobile from './mobile_action_buttons/treeButtonMobile';
 
 const ActionButton: React.FC = () => {
     const Directory_Click = () => {
@@ -37,20 +40,49 @@ const ActionButton: React.FC = () => {
         alert("I will always help you!!");
     };
 
+    const [isMobile, setIsMobile] = useState(false);
+
+    // Detect screen width on mount and resize
+    useEffect(() => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth <= 768);
+      };
+  
+      // Set initial value
+      handleResize();
+  
+      // Attach resize event listener
+      window.addEventListener('resize', handleResize);
+  
+      // Cleanup event listener on unmount
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
 
     return (
-        <div className="action-page-background">
-            <div className="action-page-upper-buttons">
-                <DirectoryButton onClick={Directory_Click} />
-                <AboutButton onClick={About_Click} />
-                <TreeButton onClick={Tree_Click} />
-                <AdminButton onClick={AdminButton_Click} />
-            </div>
-            <SearchBar onClick={SearchBar_Click} />
-            <div className="action-page-lower-buttons">
-                <MapButton onClick={MapButton_Click} />
-                <SupportButton onClick={SupportButton_Click} />
-            </div>
+        <div className="background-for-all">
+            {isMobile ? (
+                <div className='action-page-background-phone'>
+                    <div className='action-page-left-buttons'>
+                        <AdminButtonMobile onClick={AdminButton_Click} />
+                        <TreeButtonMobile onClick={Tree_Click} />
+                    </div>
+                </div>
+            ) : (
+                <div className="action-page-background">
+                    <div className="action-page-upper-buttons">
+                        <DirectoryButton onClick={Directory_Click} />
+                        <AboutButton onClick={About_Click} />
+                        <TreeButton onClick={Tree_Click} />
+                        <AdminButton onClick={AdminButton_Click} />
+                    </div>
+                    <SearchBar onClick={SearchBar_Click} />
+                    <div className="action-page-lower-buttons">
+                        <MapButton onClick={MapButton_Click} />
+                        <SupportButton onClick={SupportButton_Click} />
+                    </div>
+                </div>
+            )}
         </div>
 
     );
