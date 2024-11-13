e7f808723acd80bf93afcaffaff72b9d6d80a920
tachau03 <taminhchau8903@gmail.com>
2024-11-13 15:36:45 -0500

    When log in navigate to users

--- the-lorax/src/app/pages/login/login.tsx
+++ the-lorax/src/app/pages/login/login.tsx
@@ -1,18 +1,29 @@
 import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate for navigation
 import './login.css';
-import Header from '../../components/header'
-import treeImage from '../../../images/login-tree-image.svg'
+import Header from '../../components/header';
+import treeImage from '../../../images/login-tree-image.svg';
 import { Formik, Field, Form, ErrorMessage } from "formik";
 
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate(); // Initialize navigate function
+
+    // Check if already log in
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            navigate("/adminDashBoard"); // Redirect to admin dashboard if already logged in
+        }
+    }, [navigate]);
 
     const handleLogin = async (e: React.FormEvent) => {
         e.preventDefault();
         try {
             // Send login request to backend
-            const response = await fetch("http://localhost:8000/token", {
+            const backend = process.env.REACT_APP_FASTAPI_URL || "";
+            const response = await fetch(`${backend}token`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded"
@@ -27,10 +38,10 @@ export default function Login() {
                 const data = await response.json();
                 // Save the token for later use in other API requests
                 localStorage.setItem("token", data.access_token);
-                const token = localStorage.getItem("token");
-                console.log(token)
+                
                 alert("Login successful!");
-                // Redirect or load other content after login
+                // Redirect to admin dashboard after login
+                navigate("/adminDashBoard"); // Route to the admin dashboard
             } else {
                 alert("Login failed! Check your credentials.");
             }
@@ -38,6 +49,8 @@ export default function Login() {
             console.error("Login error:", error);
         }
     };
+
+    
     
 
     return (
