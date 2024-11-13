1faf8106efac623b1da3383284e7a35c6590a621
tachau03 <taminhchau8903@gmail.com>
2024-11-13 15:33:21 -0500

    Header to be sticky

--- the-lorax/src/app/components/header.tsx
+++ the-lorax/src/app/components/header.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import "./header.css";
 import treeSandwich from '../../images/icons/sandwichBar.svg';
 import logo from '../../images/logo.svg';
+import { Link } from 'react-router-dom';
 
 
 // Create the functional component
@@ -21,13 +22,15 @@ export default function Header() {
                 <img src={logo} className={showNav ? 'logo-icon active' : 'logo-icon'}/>
             </div>       
             <div className={showNav ? 'header-expand-bar' : "header-expand-bar active"}>
-                <text>directory</text>
-                <text>about</text>
-                <text>explore</text>
-                <text>admin</text>
-                <text>search</text>
-                <text>map</text>
-                <text>support</text>
+                <Link to="/">home</Link>
+                <Link to="/directory">directory</Link>
+                <Link to="/about">about</Link>
+                {/* <Link to="/explore">explore</Link> */}
+                <Link to="/admin">admin</Link>
+                {/* <Link to="/search">search</Link> */}
+                <Link to="/map">map</Link>
+                {/* <Link to="/support">support</Link> */}
+
             </div>
         </div>
     );
