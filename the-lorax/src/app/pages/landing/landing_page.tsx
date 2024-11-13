5d393a54f5516f988ec3d7bff4f532a02f5173a1
ntnguyen59 <jennynguyen050903@gmail.com>
2024-11-13 15:25:06 -0500

    Fix action and change to Kampus Tree

--- the-lorax/src/app/pages/landing/landing_page.tsx
+++ the-lorax/src/app/pages/landing/landing_page.tsx
@@ -2,22 +2,46 @@ import React from 'react';
 import { useEffect, useRef, useState } from "react";
 import "./landing_page.css";
 import Header  from "../../components/header";
+import HeaderMobile from '../../components/headerMobile';
   
 // Create the functional component
 
 const BackgroundComponent: React.FC = () => {
+    const [isMobile, setIsMobile] = useState(false);
+    // Detect screen width on mount and resize
+    useEffect(() => {
+        const handleResize = () => {
+          setIsMobile(window.innerWidth <= 768);
+        };
+    
+        // Set initial value
+        handleResize();
+    
+        // Attach resize event listener
+        window.addEventListener('resize', handleResize);
+    
+        // Cleanup event listener on unmount
+        return () => window.removeEventListener('resize', handleResize);
+      }, []);
 
 return (
     <div>
-        
+        {isMobile ? (
+            <div className='landing-page-background landing-page-trees'>
+                <HeaderMobile />
+                <div className="landing-page-text-container landing-page-text">
+                    Kampus Tree Project
+                </div>
+            </div>
+        ) : (
         <div className="landing-page-background landing-page-trees">
             <Header />
             <div className="landing-page-text-container landing-page-text">
-                Campus Tree Project
+                Kampus Tree Project
             </div>
         </div>
+        )}
     </div>
-    
 );
 };
   
