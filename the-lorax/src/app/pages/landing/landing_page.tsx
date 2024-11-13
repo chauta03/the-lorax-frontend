ca4348dec787cbd033f1e98b6582958f67b9be60
tachau03 <taminhchau8903@gmail.com>
2024-11-12 23:52:24 -0500

    Include header in landing-page

--- the-lorax/src/app/pages/landing/landing_page.tsx
+++ the-lorax/src/app/pages/landing/landing_page.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { useEffect, useRef, useState } from "react";
 import "./landing_page.css";
+import Header  from "../../components/header";
   
 // Create the functional component
 
 const BackgroundComponent: React.FC = () => {
 
 return (
-    <div className="landing-page-background landing-page-trees">
-        <div className="landing-page-text-container landing-page-text">
-            Campus Tree Project
+    <div>
+        
+        <div className="landing-page-background landing-page-trees">
+            <Header />
+            <div className="landing-page-text-container landing-page-text">
+                Campus Tree Project
+            </div>
         </div>
     </div>
+    
 );
 };
   
