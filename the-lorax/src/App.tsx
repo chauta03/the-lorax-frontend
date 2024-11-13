4fadc6c15280a8f4d2613a2941030bef4acc8cc1
tachau03 <taminhchau8903@gmail.com>
2024-11-13 15:37:50 -0500

    Call right name to ggmap and fix about.css

--- the-lorax/src/App.tsx
+++ the-lorax/src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import BackgroundComponent from "./app/pages/landing/landing_page";
 import ActionButton from './app/pages/action/action_page';
-import Intro from './app/page'
+import GgMap from './app/pages/ggMap/page'
 import Directory from './app/pages/directory/directory';
 import Login from './app/pages/login/login';
 import AdminDashboard from './app/pages/admin/adminDashboard';
 import About from './app/pages/about/about';
+import Header from './app/components/header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './app/flow/home';
+
 
 function App() {
+  // let Home = (props: RouteComponentProps) => <div>Home</div>
 
   return (
-    <div className="App">
-      <BackgroundComponent />
-      <ActionButton />
-      <Intro />
-      <Directory />
-      <Login/>
-      <AdminDashboard/>
-      <About />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/directory" element={<Directory />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/admin" element={<Login />} />
+        <Route path="/search" element={<Directory />} />
+        <Route path="/adminDashboard" element={<AdminDashboard />} />
+        <Route path="/map" element={<GgMap />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
