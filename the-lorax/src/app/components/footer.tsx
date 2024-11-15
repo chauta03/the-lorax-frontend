aea580636c78c9f824caa964df875e0c7fffc869
ntnguyen59 <jennynguyen050903@gmail.com>
2024-11-14 19:51:15 -0500

    Add Footer

--- the-lorax/src/app/components/footer.tsx
+++ the-lorax/src/app/components/footer.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import "./footer.css";
-import kLogo from '../../images/K-Color-1.png'
+import kLogo from '../../images/K-Color-1.png';
+import kLogoStackLeft from '../../images/K-Stack-Left-Color.png'
 
 export default function Footer() {
-    return {
-        
-    }
+    return (
+        <div className='footer'>
+            <img src={kLogoStackLeft} alt='K Logo Stack Left' className='stack-left-container'/>
+            <div className='footer-text-container'>
+                <div className='footer-text'>
+                    The Kampus Tree Project
+                </div>
+                <div className='footer-text'>
+                    Project of COMP 484/490: Computing for the Environment and Social Justice
+                </div>
+            </div>
+        </div>
+    )
 }
