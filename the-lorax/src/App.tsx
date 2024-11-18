e6ef73e7aaac8acc08a1819d3df2c808d0ea8354
tachau03 <taminhchau8903@gmail.com>
2024-11-17 22:24:25 -0500

    App new login log out status

--- the-lorax/src/App.tsx
+++ the-lorax/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import BackgroundComponent from "./app/pages/landing/landing_page";
 import ActionButton from './app/pages/action/action_page';
 import GgMap from './app/pages/ggMap/page'
 import Directory from './app/pages/directory/directory';
-import Login from './app/pages/login/login';
-import AdminDashboard from './app/pages/admin/adminDashboard';
+import Login from './app/pages/admin/admin';
 import About from './app/pages/about/about';
 import Header from './app/components/header';
 import History from './app/pages/history/history';
@@ -14,10 +14,21 @@ import Home from './app/flow/home';
 import ScrollToTop from './app/components/ScrollToTop';
 import Support from './app/pages/support/support';
 import AdminMobile from './app/pages/adminMobile/adminMobile';
+import Users from './app/pages/admin/users';
 
 
 function App() {
   // let Home = (props: RouteComponentProps) => <div>Home</div>
+  const [token, setToken] = useState<string | null>(localStorage.getItem("token"));
+
+  const handleLogin = () => {
+      setToken(localStorage.getItem("token")); // Retrieve token from localStorage after login
+  };
+
+  const handleLogout = () => {
+      localStorage.removeItem("token");
+      setToken(null);
+  };
 
   return (
     <BrowserRouter>
@@ -25,15 +36,53 @@ function App() {
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/directory" element={<Directory />} />
+        <Route path="/directory" element={
+          <>
+            <Directory token={token} />
+            {token && (
+                <button className="logout-button" onClick={handleLogout}>
+                    Logout
+                </button>
+            )}
+          </>
+        }
+        />
         <Route path="/about" element={<About />} />
-        <Route path="/admin" element={<Login />} />
-        <Route path="/search" element={<Directory />} />
-        <Route path="/adminDashboard" element={<AdminDashboard />} />
+        <Route path="/admin" element={
+            token ? (
+                 <>
+                    <Users token={token} />
+                    <button className='logout-button' onClick={handleLogout}>Logout</button>
+                 </>
+            ) : (
+                 <Login onLogin={handleLogin} />
+         )
+        } />
+        <Route path="/search" element={
+          <>
+          <Directory token={token} />
+          {token && (
+              <button className="logout-button" onClick={handleLogout}>
+                  Logout
+              </button>
+          )}
+        </>
+      }
+      />
         <Route path="/adminMobile" element={<AdminMobile />} />
         <Route path="/map" element={<GgMap />} />
         <Route path="/support" element={<Support />} />
-        <Route path="/history" element={<History />} />
+        <Route path="/history" element={
+          <>
+          <History token={token} />
+          {token && (
+              <button className="logout-button" onClick={handleLogout}>
+                  Logout
+              </button>
+          )}
+        </>
+      }
+      />
       </Routes>
     </BrowserRouter>
   );
