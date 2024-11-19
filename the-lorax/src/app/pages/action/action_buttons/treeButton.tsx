1cfe08bd7cd062c47ce83f87445675c9a028d3f5
ntnguyen59 <jennynguyen050903@gmail.com>
2024-11-19 10:54:20 -0500

    Add animation to History Button

--- the-lorax/src/app/pages/action/action_buttons/treeButton.tsx
+++ the-lorax/src/app/pages/action/action_buttons/treeButton.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import "../action_page.css";
 import { relative } from 'path';
+import sprout from '../../../../images/buttons/sprout.svg';
+import youngTree from '../../../../images/buttons/youngTree.svg';
 import tree from '../../../../images/icons/rotated-tree-icon-3-1.svg';
 import { ReactComponent as TreeBackground } from '../../../../images/buttons/treeButtonBackground.svg';
 
@@ -11,9 +14,20 @@ type CustomButtonProps = {
 };
 
 const TreeButton: React.FC = () => {
+    const images = [sprout, youngTree, tree]; // Add as many images as you want
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 1000); // Change every second
+
+        return () => clearInterval(interval); // Clean up the interval on component unmount
+    }, [images.length]);
+
     return (
         <div className="button-tree-only">
-            <img src={tree} alt="Tree Icon" className="button-tree-icon" />
+            <img src={images[currentImageIndex]} alt="Tree Icon" className="button-tree-icon" />
             <TreeBackground className="button-tree-background" />
         </div>
     );
