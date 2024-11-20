cd574d7cada8bae8c69bb98c436eaad59328b55c
tachau03 <taminhchau8903@gmail.com>
2024-11-20 13:34:01 -0500

    add our names on there!

--- the-lorax/src/app/pages/about/about.tsx
+++ the-lorax/src/app/pages/about/about.tsx
@@ -37,6 +37,9 @@ export default function About() {
                         The Campus Tree Project is an attempt by K students to appreciate our campus's hidden heroes and companions. 
                         In collaboration with Dr. Binney Girdler of the Biology Department, we digitized the locations of (almost) every tree on our main campus, which was collected in two previous Seniors Integrated Projects. With the Campus Tree Project, you can locate, search, and learn more about every tree you encounter. Our goal is to monitor the carbon sequestration of trees on campus, in the spirit of the Climate Action Plan of Kalamazoo College.
                         </text>
+                        <text className="body-text">
+                        We are the Lorax who speaks for the tree... This website is created by Alex Kish 25', Cole Koryto 25', Jenny Nguyen 25', and Chau Ta 25'.
+                        </text>
                     </div>
                 </div>
             ) : (
@@ -54,6 +57,9 @@ export default function About() {
                         <text className="body-text">
                         The Campus Tree Project is an attempt by K students to appreciate our campus's hidden heroes and companions. 
                         In collaboration with Dr. Binney Girdler of the Biology Department, we digitized the locations of (almost) every tree on our main campus, which was collected in two previous Seniors Integrated Projects. With the Campus Tree Project, you can locate, search, and learn more about every tree you encounter. Our goal is to monitor the carbon sequestration of trees on campus, in the spirit of the Climate Action Plan of Kalamazoo College.
+                        </text>
+                        <text className="body-text">
+                        We are the Lorax who speaks for the tree... This website is created by Alex Kish 25', Cole Koryto 25', Jenny Nguyen 25', and Chau Ta 25'.
                         </text>
                     </div>
                 </div>
